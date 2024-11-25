a9cc7a45f38dc94552907c762cef3636c08616ce
mauantonelli <pablojuanang@gmail.com>
2024-11-25 16:04:49 -0300

    métodos fora do escopo

--- src/main/java/adapters/dao/ImovelDAO.java
+++ src/main/java/adapters/dao/ImovelDAO.java
@@ -1,6 +1,10 @@
 package adapters.dao;
 
 import domain.entity.Imovel;
+import domain.entity.Proprietario;
+
+import java.util.List;
 
 public interface ImovelDAO extends DAO<Imovel> {
+    List<Imovel> obterImovelPorProprietario(Proprietario proprietario);
 }
--- src/main/java/adapters/dao/LocacaoDAO.java
+++ src/main/java/adapters/dao/LocacaoDAO.java
@@ -1,6 +1,13 @@
 package adapters.dao;
 
+import domain.entity.Inquilino;
 import domain.entity.Locacao;
+import domain.entity.Proprietario;
+import java.util.List;
 
 public interface LocacaoDAO extends DAO<Locacao>{
+
+    List<Locacao> obterLocacaoPorProprietario(Proprietario proprietario);
+    List<Locacao> obterLocacaoPorInquilino(Inquilino inquilino);
+
 }
