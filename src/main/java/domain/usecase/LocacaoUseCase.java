31f6138988ecf6b4b571d86199453545588b7fa7
mauantonelli <pablojuanang@gmail.com>
2024-11-27 18:35:02 -0300

    usecase methods

--- src/main/java/domain/usecase/LocacaoUseCase.java
+++ src/main/java/domain/usecase/LocacaoUseCase.java
@@ -1,11 +1,27 @@
 package domain.usecase;
 
+import domain.entity.Locacao;
 import domain.repository.LocacaoRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public class LocacaoUseCase {
     private final LocacaoRepository repository;
 
     public LocacaoUseCase(LocacaoRepository repository) {
         this.repository = repository;
+    }
+
+    public void salvar(Locacao locacao){
+        repository.salvar(locacao);
+    }
+
+    public List<Locacao> obterTodos(){
+        return repository.obterTodos();
+    }
+
+    Optional<Locacao> obterPorId(int id){
+        return repository.obterId(id);
     }
 }
