1f4912d7c3f3df0983f461ed5c0382012360a473
mauantonelli <pablojuanang@gmail.com>
2024-11-28 09:10:55 -0300

    save prop

--- src/main/java/adapters/dao/ProprietarioDAOimpl.java
+++ src/main/java/adapters/dao/ProprietarioDAOimpl.java
@@ -1,14 +1,35 @@
 package adapters.dao;
 
+import adapters.ConnectionFactory;
 import domain.entity.Proprietario;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class ProprietarioDAOimpl implements  ProprietarioDAO {
     @Override
-    public int salvar(Proprietario type) {
-        return 0;
+    public int salvar(Proprietario proprietario) {
+        String sql = "INSERT INTO proprietario (id, nome, telefone) values (?,?,?)";
+        int generatedId = 0;
+
+        try (PreparedStatement stmt = ConnectionFactory.createStatement(sql)) {
+            stmt.setInt(1, proprietario.getId());
+            stmt.setString(2, proprietario.getNome());
+            stmt.setString(3, proprietario.getTelefone());
+            stmt.executeUpdate();
+
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next())
+                generatedId = generatedKeys.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return generatedId;
+
     }
 
     @Override
