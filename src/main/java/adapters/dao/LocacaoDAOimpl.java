2b90bb39504a72341636a2550b8cf53c1e147950
mauantonelli <pablojuanang@gmail.com>
2024-11-28 09:08:40 -0300

    locacao save

--- src/main/java/adapters/dao/LocacaoDAOimpl.java
+++ src/main/java/adapters/dao/LocacaoDAOimpl.java
@@ -1,16 +1,37 @@
 package adapters.dao;
 
+import adapters.ConnectionFactory;
 import domain.entity.Inquilino;
 import domain.entity.Locacao;
 import domain.entity.Proprietario;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class LocacaoDAOimpl implements LocacaoDAO{
     @Override
-    public int salvar(Locacao type) {
-        return 0;
+    public int salvar(Locacao locacao) {
+        String sql = "INSERT INTO locacao (id, dataInicio, dataFim, valorAluguel) values (?,?,?,?)";
+
+        int generatedId = 0;
+        try (PreparedStatement stmt = ConnectionFactory.createStatement(sql)) {
+            stmt.setInt(1, locacao.getId());
+            stmt.setString(2, locacao.getDataInicio());
+            stmt.setString(3, locacao.getDataFim());
+            stmt.setDouble(4, locacao.getValorAluguel());
+            stmt.executeUpdate();
+
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next())
+                generatedId = generatedKeys.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return generatedId;
     }
 
     @Override
