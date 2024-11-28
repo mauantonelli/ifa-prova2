a8ad2d416cabebe97654dd704bca07b3262ca780
mauantonelli <pablojuanang@gmail.com>
2024-11-27 23:00:08 -0300

    thiftami save into inquilinoDAOimpl

--- src/main/java/adapters/dao/InquilinoDAOimpl.java
+++ src/main/java/adapters/dao/InquilinoDAOimpl.java
@@ -1,14 +1,35 @@
 package adapters.dao;
 
+import adapters.ConnectionFactory;
 import domain.entity.Inquilino;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class InquilinoDAOimpl implements InquilinoDAO {
     @Override
-    public int salvar(Inquilino type) {
-        return 0;
+    public int salvar(Inquilino inquilino) {
+        String sql = "INSET INTO inqulino (id, nome, cpf) values (?,?,?)";
+        int generatedId = 0;
+        try (PreparedStatement stmt = ConnectionFactory.createStatement(sql)) {
+            stmt.setInt(1, inquilino.getId());
+            stmt.setString(2, inquilino.getNome());
+            stmt.setString(3, inquilino.getCpf());
+            stmt.executeUpdate();
+
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next())
+                generatedId = generatedKeys.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return generatedId;
+
+
     }
 
     @Override
